32f51eb7b36b589b7e4f9241d621ece55e104fd1
amf673 <afedoruk@mtroyal.ca>
2024-01-23 09:07:47 -0700

    Update variables.py

--- variables.py
+++ variables.py
@@ -16,9 +16,47 @@ d = a * c
 # - A type - find the type with type()
 # - A current value (find with print()
 
-print("Varaible a has value", a, "and type", type(a))
-print("Varaible b has value", b, "and type", type(b))
-print("Varaible c has value", c, "and type", type(c))
-print("Varaible d has value", d, "and type", type(d))
+print()
+print("Variable a has value", a, "and type", type(a))
+print("Variable b has value", b, "and type", type(b))
+print("Variable c has value", c, "and type", type(c))
+print("Variable d has value", d, "and type", type(d))
 
+# You can reuse variables but it is considered bad form to change the type mid program. 
+#
+a = 6
+print()
+print("Variable a has value", a, "and type", type(a))
+print("Variable b has value", b, "and type", type(b))
+print("Variable c has value", c, "and type", type(c))
+print("Variable d has value", d, "and type", type(d)) # notice that this did not change. d = a * c is not an equivalence, but an assignment. 
 
+# Casting 
+
+e = int(c) # int() converts its argument to an integer value. If the argument is a float it truncates it. 
+print()
+print("Variable e has value", c, "and type", type(c))
+print("Variable e has value", e, "and type", type(e))
+
+f = float(a)
+print()
+print("Variable a has value", a, "and type", type(a))
+print("Variable f has value", f, "and type", type(f))
+
+# Casting from numeric to string 
+
+g = str(a)
+h = str(c)
+
+print()
+print("Variable g has value", g, "and type", type(g))
+print("Variable h has value", h, "and type", type(h))
+
+# Casting from numeric to string 
+
+i = input("Enter a number: ")
+j = float(i) # this only works if the string represents a valid number
+
+print()
+print("Variable i has value", i, "and type", type(i))
+print("Variable j has value", j, "and type", type(j))
