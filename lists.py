293539a743a6f89c631d5153eeec61edcf25f3f5
amf673 <afedoruk@mtroyal.ca>
2024-03-19 14:52:17 -0600

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,32 +1,58 @@
+# 
 # Lists
+# COMP 1701 
+# A. Fedoruk
 
+# Some examples of working with lists. 
 
-a = ["Calgary", "Edmonton", "Red Deer", "Lethbridge", "Medicine Hat", "Airdrie", "Coleman", "Fort MacMurray"]
-b = [1300000, 1000000, 100000, 90000, 80000, 50000, 10000, 30000]
+cities = ["Calgary", "Edmonton", "Red Deer", 
+          "Lethbridge", "Medicine Hat", "Airdrie", 
+          "Coleman", "Fort MacMurray", "Edson"]
 
-print(a)
+print(cities)
 
-# indexing
+# 1. Indexing
+#
+# Lists are indexed from 0 to n-1. 
+# To find out how many items are in a list, use len()
 
-print( a[0])
-l = len(a)
-print(a[l-1])
+number_of_cities = len(cities)
+print(f"There are {number_of_cities} cities in the list")
 
-# sorting a list
-a.sort()
-print(a)
+# To access individual members of the list use the index [i]
 
-# adding to a list 
+city = cities[0] # the first city 
+city2 = cities[number_of_cities - 1] # the last city
+
+# Iterating through the list of cities 
+
+for i in range(0, len(cities)):
+    print(cities[i])
+
+# 2. Adding and deleting from lists
+
+# .append() adds an item to the end of the list. 
+
 new_city = input("Enter a city: ")
-a.append(new_city)
+cities.append(new_city)
+print(cities)
 
-a.remove("Lethbridge")
-print(a)
-city = a.pop(3)
-print(city)
-print(a)
+# .remove() will remove an item from the list. 
 
-new_city = input("Enter a city: ")
-if new_city in a: 
-    print(new_city, "is in the list")
-    print(new_city, "Is the ", a.index(new_city), "Element")
+remove_city = input("Enter city to remove: ")
+cities.remove(remove_city)
+
+# 3. List membership 
+# You can use the in keyword 
+
+city_to_find = input("Enter a city: ")
+if city_to_find in cities: 
+    print("The city is in the list.")
+else: 
+    print("Not found.")
+
+# 4. sorting a list
+# Python provides a sort method. 
+
+cities.sort()
+print(cities)
